f7daca5607a8d24be46e231a528aaed4f57e5612
ckyeon <qsky8520@gmail.com>
2022-09-15 23:36:26 +0900

    Add Auth Repository

---
+++ src/main/java/com/sequence/proreviewer/auth/infra/repository/AuthRepository.java
@@ -0,0 +1,13 @@
+package com.sequence.proreviewer.auth.infra.repository;
+
+import com.sequence.proreviewer.auth.domain.Auth;
+import java.util.Optional;
+
+public interface AuthRepository {
+
+	Optional<Auth> findById(Long id);
+
+	Optional<Auth> findByProviderKey(String key);
+
+	Auth saveAuth(Auth auth);
+}
---
+++ src/main/java/com/sequence/proreviewer/auth/infra/repository/jpa/JpaAuthRepository.java
@@ -0,0 +1,10 @@
+package com.sequence.proreviewer.auth.infra.repository.jpa;
+
+import com.sequence.proreviewer.auth.domain.Auth;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaAuthRepository extends JpaRepository<Auth, Long> {
+
+	Optional<Auth> findByProviderKey(String key);
+}

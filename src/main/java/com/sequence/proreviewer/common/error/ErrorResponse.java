74509903e458f9e91441fc06cbf494ab8ed5d268
ckyeon <qsky8520@gmail.com>
2022-09-18 02:31:14 +0900

    Add ErrorResponse for exception handling

--- src/main/java/com/sequence/proreviewer/common/error/ErrorResponse.java
+++ src/main/java/com/sequence/proreviewer/common/error/ErrorResponse.java
@@ -1,2 +1,15 @@
-package com.sequence.proreviewer.common.error;public class ErrorResponse {
+package com.sequence.proreviewer.common.error;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+	private int status;
+	private String message;
+
+	public ErrorResponse(ErrorCode errorCode) {
+		this.status = errorCode.getStatus();
+		this.message = errorCode.getMessage();
+	}
 }

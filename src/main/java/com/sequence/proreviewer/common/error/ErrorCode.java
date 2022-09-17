3ba749aeaf29ecb84f13bf7a4bb3a49c678174a7
ckyeon <qsky8520@gmail.com>
2022-09-18 02:31:30 +0900

    Add ErrorCode enum

--- src/main/java/com/sequence/proreviewer/common/error/ErrorCode.java
+++ src/main/java/com/sequence/proreviewer/common/error/ErrorCode.java
@@ -1,16 +1,16 @@
-package com.sequence.proreviewer.common;
+package com.sequence.proreviewer.common.error;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
+@AllArgsConstructor
 @Getter
 public enum ErrorCode {
-	INVALID_AUTHORIZATION_CODE(401, "INVALID_AUTHORIZATION_CODE");
+	INVALID_GITHUB_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED.value(), "INVALID_GITHUB_ACCESS_TOKEN"),
+	INVALID_AUTHORIZATION_CODE(HttpStatus.UNAUTHORIZED.value(), "INVALID_AUTHORIZATION_CODE");
+
 
 	private final int status;
 	private final String message;
-
-	ErrorCode(int status, String message) {
-		this.status = status;
-		this.message = message;
-	}
 }

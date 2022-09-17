7f5fc21b99adc9eca602db6fe5f792cb7b6e6bbe
ckyeon <qsky8520@gmail.com>
2022-09-18 02:37:08 +0900

    Add InvalidGithubAccessTokenException for github exception handling

--- src/main/java/com/sequence/proreviewer/auth/application/exception/InvalidGithubAccessTokenException.java
+++ src/main/java/com/sequence/proreviewer/auth/application/exception/InvalidGithubAccessTokenException.java
@@ -1,2 +1,11 @@
-package com.sequence.proreviewer.auth.application.exception;public class BadCredentialsException {
+package com.sequence.proreviewer.auth.application.exception;
+
+import com.sequence.proreviewer.common.error.BaseCustomException;
+import com.sequence.proreviewer.common.error.ErrorCode;
+
+public class InvalidGithubAccessTokenException extends BaseCustomException {
+
+	public InvalidGithubAccessTokenException() {
+		super(ErrorCode.INVALID_GITHUB_ACCESS_TOKEN);
+	}
 }

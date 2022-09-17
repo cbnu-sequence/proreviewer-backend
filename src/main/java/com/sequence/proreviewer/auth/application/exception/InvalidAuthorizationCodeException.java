8271e9cd8b950081c2b2c2cc959b7ace9865de36
ckyeon <qsky8520@gmail.com>
2022-09-18 02:36:36 +0900

    Add InvalidAuthorizationCodeException for github exception handling

--- src/main/java/com/sequence/proreviewer/auth/application/exception/InvalidAuthorizationCodeException.java
+++ src/main/java/com/sequence/proreviewer/auth/application/exception/InvalidAuthorizationCodeException.java
@@ -1,2 +1,11 @@
-package com.sequence.proreviewer.auth.application.exception;public class InvalidAuthorizationCodeException {
+package com.sequence.proreviewer.auth.application.exception;
+
+import com.sequence.proreviewer.common.error.BaseCustomException;
+import com.sequence.proreviewer.common.error.ErrorCode;
+
+public class InvalidAuthorizationCodeException extends BaseCustomException {
+
+	public InvalidAuthorizationCodeException() {
+		super(ErrorCode.INVALID_AUTHORIZATION_CODE);
+	}
 }

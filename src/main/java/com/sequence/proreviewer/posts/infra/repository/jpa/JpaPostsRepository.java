6a19b4a3d609db898e58bafa3b7d185ab4acce2e
mk3058 <kmk3058@icloud.com>
2022-09-18 16:39:43 +0900

    게시글 등록 기능 추가

---
+++ src/main/java/com/sequence/proreviewer/posts/infra/repository/jpa/JpaPostsRepository.java
@@ -0,0 +1,14 @@
+package com.sequence.proreviewer.posts.infra.repository.jpa;
+
+import com.sequence.proreviewer.posts.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
+
+public interface JpaPostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p"+
+            "FROM Posts p"+
+            "ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
+}

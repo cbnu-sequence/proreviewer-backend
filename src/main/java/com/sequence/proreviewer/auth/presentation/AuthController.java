bef2344448bd8fa47f68cb8315869d546df4888c
ckyeon <qsky8520@gmail.com>
2022-09-18 17:48:39 +0900

    Add google method for google social login

--- src/main/java/com/sequence/proreviewer/auth/presentation/AuthController.java
+++ src/main/java/com/sequence/proreviewer/auth/presentation/AuthController.java
@@ -3,10 +3,13 @@ package com.sequence.proreviewer.auth.presentation;
 import com.sequence.proreviewer.auth.application.AuthService;
 import com.sequence.proreviewer.auth.presentation.dto.request.LoginRequestDto;
 import com.sequence.proreviewer.auth.presentation.dto.response.AuthTokens;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
 	private final AuthService authService;
+
+	@PostMapping("/login/google")
+	public AuthTokens google(@RequestBody LoginRequestDto dto) {
+		return authService.googleLogin(dto);
+	}
 
 	@PostMapping("/login/github")
 	public AuthTokens github(@RequestBody LoginRequestDto dto) {

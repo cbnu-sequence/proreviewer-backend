358d9fcc13c6c452ca4ed02613d03af3de6e73a0
ckyeon <qsky8520@gmail.com>
2022-09-19 11:02:20 +0900

    Add WebMvcConfig for register HttpLoggingInterceptor

--- src/main/java/com/sequence/proreviewer/common/config/WebMvcConfig.java
+++ src/main/java/com/sequence/proreviewer/common/config/WebMvcConfig.java
@@ -1,2 +1,21 @@
-package com.sequence.proreviewer.common.config;public class WebMvcConfig {
+package com.sequence.proreviewer.common.config;
+
+import com.sequence.proreviewer.common.logging.HttpLoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final HttpLoggingInterceptor httpLoggingInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(httpLoggingInterceptor)
+			.excludePathPatterns("**/");
+	}
 }

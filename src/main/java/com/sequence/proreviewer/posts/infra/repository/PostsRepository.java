f5cc65918b66d76b8ff458ad0a49687cf5b7472d
mk3058 <kmk3058@icloud.com>
2022-09-19 19:54:41 +0900

    게시글 조회, 수정, 삭제 구현

--- src/main/java/com/sequence/proreviewer/posts/infra/repository/PostsRepository.java
+++ src/main/java/com/sequence/proreviewer/posts/infra/repository/PostsRepository.java
@@ -2,11 +2,19 @@ package com.sequence.proreviewer.posts.infra.repository;
 
 import com.sequence.proreviewer.posts.domain.Posts;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public interface PostsRepository {
     void savePosts(Posts posts);
+
     void deleteAllPosts();
 
     Stream<Posts> getAllPosts();
+
+    Optional<Posts> findById(Long id);
+
+    void editPosts(Posts posts);
+
+    void deletePosts(Long id);
 }

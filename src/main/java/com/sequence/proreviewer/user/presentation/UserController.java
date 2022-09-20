411a4f23930f2609ccc217cfcfad5ee39641d518
ckyeon <qsky8520@gmail.com>
2022-09-20 10:08:57 +0900

    Add UserController for User - Read, Update, Delete

--- src/main/java/com/sequence/proreviewer/user/presentation/UserController.java
+++ src/main/java/com/sequence/proreviewer/user/presentation/UserController.java
@@ -1,2 +1,12 @@
-package com.sequence.proreviewer.user.presentation;public class UserController {
+package com.sequence.proreviewer.user.presentation;
+
+import com.sequence.proreviewer.user.application.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
 }

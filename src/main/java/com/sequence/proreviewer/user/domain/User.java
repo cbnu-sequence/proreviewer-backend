c11e3a95616ead6ab69452a3fc50d2d9c7504e53
ckyeon <qsky8520@gmail.com>
2022-09-20 09:26:56 +0900

    Fix RequiredArgsConstructor to protected default constructor

--- src/main/java/com/sequence/proreviewer/user/domain/User.java
+++ src/main/java/com/sequence/proreviewer/user/domain/User.java
@@ -6,13 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
 	@Id

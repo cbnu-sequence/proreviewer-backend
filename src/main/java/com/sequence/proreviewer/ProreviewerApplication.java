f36739b5f1fc878305f74bb2ae7cff34c242d04a
ckyeon <qsky8520@gmail.com>
2022-10-02 08:32:02 +0900

    Test gradle build failed

--- src/main/java/com/sequence/proreviewer/ProreviewerApplication.java
+++ src/main/java/com/sequence/proreviewer/ProreviewerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProreviewerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProreviewerApplication.class, args);
+		SpringApplication.run(aaaa.class, args);
 	}
 
 }

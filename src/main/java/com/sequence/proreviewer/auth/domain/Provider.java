277737bb3b8047d3fcd3d204024aef0bba01550a
ckyeon <qsky8520@gmail.com>
2022-10-25 19:49:38 +0900

    Add @Getter to Provider

--- src/main/java/com/sequence/proreviewer/auth/domain/Provider.java
+++ src/main/java/com/sequence/proreviewer/auth/domain/Provider.java
@@ -3,7 +3,9 @@ package com.sequence.proreviewer.auth.domain;
 import com.sequence.proreviewer.auth.application.oauth2.GithubOAuth2;
 import com.sequence.proreviewer.auth.application.oauth2.GoogleOAuth2;
 import com.sequence.proreviewer.auth.application.oauth2.OAuth2;
+import lombok.Getter;
 
+@Getter
 public enum Provider {
     GOOGLE(GoogleOAuth2.class),
     GITHUB(GithubOAuth2.class);

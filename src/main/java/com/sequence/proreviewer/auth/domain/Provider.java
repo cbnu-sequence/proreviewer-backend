6afa859e559095e3909ad4e8f4002681d13a2b47
ckyeon <qsky8520@gmail.com>
2022-10-25 19:48:18 +0900

    Add oAuth2ConcreteClass constant value to Provider

--- src/main/java/com/sequence/proreviewer/auth/domain/Provider.java
+++ src/main/java/com/sequence/proreviewer/auth/domain/Provider.java
@@ -1,6 +1,16 @@
 package com.sequence.proreviewer.auth.domain;
 
+import com.sequence.proreviewer.auth.application.oauth2.GithubOAuth2;
+import com.sequence.proreviewer.auth.application.oauth2.GoogleOAuth2;
+import com.sequence.proreviewer.auth.application.oauth2.OAuth2;
+
 public enum Provider {
-    GOOGLE,
-    GITHUB
+    GOOGLE(GoogleOAuth2.class),
+    GITHUB(GithubOAuth2.class);
+
+    private final Class<? extends OAuth2> oAuth2ConcreteClass;
+
+    Provider(Class<? extends OAuth2> oAuth2ConcreteClass) {
+        this.oAuth2ConcreteClass = oAuth2ConcreteClass;
+    }
 }
